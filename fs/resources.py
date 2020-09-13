448dd2545e1e1bae788fdc7d5716a13cfe114bff
Ale <1374688@gmail.com>
2020-09-13 16:34:20 +0800

    added name-based subfolders

--- fs/resources.py
+++ fs/resources.py
@@ -8,7 +8,11 @@ from flask import request, jsonify
 
 class Uploader(Resource):
 
+    def __init__(self, filedir):
+        self.filedir = filedir
+
     def hasher(self, file_obj):
+
         BLOCK_SIZE = 65536 #64Kb
         file_hash = hashlib.sha256() 
         fb = file_obj.read(BLOCK_SIZE)
@@ -16,23 +20,13 @@ class Uploader(Resource):
             file_hash.update(fb)
             fb = file_obj.read(BLOCK_SIZE)
         file_obj.seek(0)
-        print('HASH!!!111', file_hash.hexdigest())
         return file_hash.hexdigest()
 
 
-
-        # with open(filename, 'rb') as f:
-        #     fb = f.read(BLOCK_SIZE)
-        #     while len(fb) > 0:
-        #         file_hash.update(fb)
-        #         fb = f.read(BLOCK_SIZE)
-        
-        # print('HASH!!!111', file_hash.hexdigest())
-        # return file_hash.hexdigest()
+    def saver(self, file_obj):
+        pass
 
 
-    def __init__(self, filedir):
-        self.filedir = filedir
 
     def post(self):
         # curl -X POST 127.0.0.1:4000/api/upload -F 'file=@testfiles/test.txt' -i
@@ -46,10 +40,17 @@ class Uploader(Resource):
             # submitted_file_name = submitted_file.filename
 
             submitted_file_name = self.hasher(submitted_file)
+            # submitted_file_name = '71submitted_1cf311a8ce4d57289d6faa93d0c43809a8b5b14f9b9d66f013af'
 
-            submitted_file.save(Path(self.filedir) / submitted_file_name)
+            subdir_name = submitted_file_name[0:2]
+            subdir_path = Path(self.filedir) / subdir_name
+            if not (subdir_path.exists() and subdir_path.is_dir()):
+                Path(subdir_path).mkdir()
+            
+            submitted_file.save(Path(subdir_path) / submitted_file_name)
             out = {
                 'status': 'OK',
+                'path': str(Path(subdir_path)),
                 'filename': submitted_file_name,
                 'message': f"{submitted_file_name} saved successful."
                 }
