e9ebd3e06c94d18e72bd19020e308e0c5377ac4b
Ale <1374688@gmail.com>
2020-09-20 15:38:27 +0800

    added doc and comments to __init__

--- fs/__init__.py
+++ fs/__init__.py
@@ -6,15 +6,30 @@ import logging.handlers
 import logging
 import os
 
+''' Set up application.
 
+- Logging.
+- Selest config (production, development, or testing).
+- Verify that folder for uploading files is accessible.
+- Import API resources.
+
+Create application object and api object. 
+
+Raise:
+        FileExistsError: The upload directory name is already taken by a file.
+
+'''
+
+
+# Set up logging.
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# Name of log file is taken from env variable LOGFILE, defaults to 
+# fs.log if the variable was not set.
 file_handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", "fs.log"))
 file_handler.setLevel(logging.DEBUG)
-
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
@@ -22,8 +37,10 @@ logger.addHandler(file_handler)
 app = Flask(__name__)
 logger.info('App initiated.')
 
-# export FLASK_ENV variable
 
+
+
+# Export FLASK_ENV variable to set config.
 if os.environ.get("FLASK_ENV") == "production":
     app.config.from_object("config.Config")
     logger.debug('Production config loaded.')
@@ -37,23 +54,29 @@ else:
 
 filedir = Path(__file__).parent.parent / (app.config['UPLOAD_FOLDER'])
 
-# verify that upload directory exists:
+# Verify that upload directory exists:
 if not (filedir.exists() and filedir.is_dir()):
     try:
+        # create, if it doesn't
         Path(filedir).mkdir()
         logger.info('Upload folder created.')
     except FileExistsError:
+        # raise exception if dir name conflict with a file.
         err_message = f"File named \"{app.config['UPLOAD_FOLDER']}\" already exists. Rename the file or set a different name for the upload folder in config.py."
         logger.critical("Exception. " + err_message)
-        raise Exception(err_message)
+        raise FileExistsError(err_message)
 
 
 api = Api(app)
 
+# Import resource that handles file upload.
 from fs.resources import Uploader
 api.add_resource(Uploader, '/api/upload', endpoint='uploader', \
     resource_class_kwargs={'filedir': filedir})
 
+# Import resource that handles download and delete. Same URL is used 
+# in both cases, action depends on HTTP method: GET for download,
+# DELETE for delete.
 from fs.resources import File
 api.add_resource(File, '/api/file/<file_name>', endpoint='file', \
     resource_class_kwargs={'filedir': filedir})
